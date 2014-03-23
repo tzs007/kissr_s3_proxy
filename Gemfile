a94a8b19d45c5d178e4e792e2d2c35055ec72809
Mason Fischer <mason@thoughtbot.com>
2014-03-23 00:54:51 -0400

    Add AWS

--- Gemfile
+++ Gemfile
@@ -1,3 +1,8 @@
 source 'https://rubygems.org'
 
+gem 'rails'
+gem 'dotenv'
+gem 'haml'
+gem 'fog'
 gem 'goliath'
+gem 'pry'
--- proxy.rb
+++ proxy.rb
@@ -1,8 +1,60 @@
-require 'goliath'
+require 'dotenv'
+Dotenv.load
+
+require 'bundler/setup'
+require 'action_view'
+Bundler.require(:default)
+require 'action_dispatch'
+require 'active_support'
 
 class Proxy < Goliath::API
+  include Goliath::Rack::Templates 
+
   def response(env)
-    [200, {}, "Hello World"]
+    host = ENV['RACK_ENV'] == 'production'  ?
+      env['SERVER_NAME'] : 'test.kissr.com'
+    bucket = aws.directories.new(key: ENV['KISSR_BUCKET'])
+
+    request = env['REQUEST_PATH']
+
+    if request[-1, 1] == '/'
+      request += 'index.html'
+    end
+
+    file = bucket.files.get("#{host}#{request}")
+
+    if file.present?
+      content_type = mime_type(File.extname(request)[1..-1])
+
+      [
+        200,
+        {"Content-Type" => content_type },
+        file.body
+      ]
+    else
+      [
+        404,
+        {},
+        haml(:not_found)
+      ]
+    end
+  end
+
+  def mime_type(extention)
+    extentions = {'htm' => "text/html"}
+    if extentions[extention]
+      extentions[extention] 
+    else
+      Mime::Type.lookup_by_extension(extention).to_s
+    end
+  end
+
+  def aws
+    @aws ||= Fog::Storage.new(
+      provider: 'AWS',
+      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+    )
   end
 end
 
